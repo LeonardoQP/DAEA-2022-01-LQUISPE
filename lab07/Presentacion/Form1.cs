8d4bbdd9b3bbd0864b2465116a9e15e0629cf4ac
unknown <leonardo.quispe@tecsup.edu.pe>
2022-07-04 00:01:15 -0500

    lab07

---
+++ lab07/Presentacion/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Negocio;
+
+namespace Presentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            clsNegPerson np = new clsNegPerson();
+            dt = np.GetAll();
+
+            dgDatos.DataSource = dt;
+            dgDatos.Refresh();
+        }
+
+        private void dgDatos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string data = txtNombre.Text;
+            DataTable dt = new DataTable();
+            clsNegPerson np = new clsNegPerson();
+            dt = np.GetBuscar(data);
+            dgDatos.DataSource = dt;
+            dgDatos.Refresh();
+        }
+    }
+}
